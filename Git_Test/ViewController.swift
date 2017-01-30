d30e1ef69334c58f53bd7b8748cdc6f3753ea78b
puneet1710 <puneet.gurtoo@sparxitsolutions.com>
2017-01-30 14:28:57 +0530

    First commit

---
+++ Git_Test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Git_Test
+//
+//  Created by sismac009 on 30/01/17.
+//  Copyright © 2017 sismac009. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("first")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
